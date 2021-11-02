4cff0e06d0857f2cde88844f7a10c2d9ea4917d1
simone codeluppi <simone.codeluppi@gmail.com>
2021-11-02 14:43:03 +0100

    Uploaded data

---
+++ general_processing_docker_file/start_commands.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+jupyter lab --ServerApp.token='analysis' --port=8080 --ip=0.0.0.0 --no-browser --allow-root 
+
+
+# --ServerApp.allow_password_change=True
---
+++ notes.md
@@ -0,0 +1,5 @@
+# Notes on the analysis
+
+![image1](/Marcos_IF_analysis_fig/IF_mask_original.png)
+
+
